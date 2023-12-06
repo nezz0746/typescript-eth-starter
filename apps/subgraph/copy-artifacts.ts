d625fad7dc098161e72363f9c5ee8150f0a6cbf1
Nezzar KEFIF <36443340+nezz0746@users.noreply.github.com>
2023-12-06 12:09:59 +0100

    feat: update subgraph artifact copy & deployment scripts

--- apps/subgraph/copy-artifacts.ts
+++ apps/subgraph/copy-artifacts.ts
@@ -4,29 +4,51 @@ import fs from "fs/promises";
 const chainIdToNetwork = {
   1337: "localhost",
   5: "goerli",
+  80001: "mumbai",
   84531: "base-testnet",
 };
 
-const main = async () => {
-  for (const [chainId, address] of Object.entries(counterAddress)) {
-    await fs.writeFile(
-      `./config/${chainId}.json`,
-      JSON.stringify(
-        {
-          counterAddress: address,
-          startBlock: 0,
-          network: chainIdToNetwork[chainId],
-        },
-        null,
-        2
-      )
-    );
-  }
+/**
+ * This is a list of all the artifacts we want to copy over to the subgraph
+ * or any other package that needs them.
+ */
+const artifacts = [
+  {
+    name: "Counter",
+    abi: counterABI,
+    addresses: counterAddress,
+    chainIds: Object.keys(counterAddress).map((chainId) => parseInt(chainId)),
+  },
+] as const;
 
-  await fs.writeFile(
-    "./abis/Counter.json",
-    JSON.stringify(counterABI, null, 2)
-  );
+const getDeploymentStartBlock = async (name: string, chainId: number) => {
+  return fs
+    .readFile(`../contracts/deployments/${chainId}/${name}.json`, "utf-8")
+    .then(JSON.parse)
+    .then((dep) => dep.startBlock);
+};
+
+const main = async () => {
+  for (const artifact of artifacts) {
+    for (const chainId of artifact.chainIds) {
+      await fs.writeFile(
+        `./config/${chainId}.json`,
+        JSON.stringify(
+          {
+            counterAddress: artifact.addresses[chainId],
+            startBlock: await getDeploymentStartBlock(artifact.name, chainId),
+            network: chainIdToNetwork[chainId],
+          },
+          null,
+          2
+        )
+      );
+      await fs.writeFile(
+        `./abis/${artifact.name}.json`,
+        JSON.stringify(artifact.abi, null, 2)
+      );
+    }
+  }
 };
 
 main();
